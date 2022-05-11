2040385066ae007ec9468919fd57aaaed1021ac6
Gustavo Freitas <gussdelf@gmail.com>
2022-05-11 18:53:16 -0300

    format

--- configuration.nix
+++ configuration.nix
@@ -1,29 +1,36 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix <home-manager/nixos> ];
+  imports = [ /etc/nixos/hardware-configuration.nix ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
-
-  networking.hostName = "nixos";
+  boot.loader = {
+    grub.enable = true;
+    grub.version = 2;
+    grub.device = "/dev/sda";
+  };
 
   time.timeZone = "America/Sao_Paulo";
-  networking.useDHCP = false;
-  networking.interfaces.enp0s29f0u2.useDHCP = true;
-  networking.interfaces.enp1s0.useDHCP = true;
+
+  networking = {
+    useDHCP = false;
+    interfaces.enp0s29f0u2.useDHCP = true;
+    interfaces.enp1s0.useDHCP = true;
+    hostName = "boo";
+  };
 
   i18n.defaultLocale = "pt_BR.UTF-8";
   console = { keyMap = "br-abnt2"; };
 
   nixpkgs.config.allowUnfree = true;
 
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+  };
 
   services.xserver = {
     enable = true;
+    layout = "br";
     displayManager.startx.enable = true;
     windowManager.bspwm.enable = true;
     windowManager.awesome.enable = true;
@@ -39,10 +46,10 @@
   ];
   nixpkgs.config.joypixels.acceptLicense = true;
 
-  services.xserver.layout = "br";
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
+
   programs.adb.enable = true;
 
   users.users.gengar = {
@@ -59,76 +66,77 @@
 
   nixpkgs.overlays = [
     (import ./overlay.nix)
-    #(import (builtins.fetchTarball {
-    #  url =
-    #    "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
-    #}))
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+    }))
   ];
 
-  environment.systemPackages = with pkgs; [
-    zsh
-    fish
-    ripgrep
-    sxhkd
-    polybar
-    fd
-    fzf
-    go
-    gopls
-    sumneko-lua-language-server
-    brave
-    firefox
-    exa
-    starship
-    lazygit
-    git
-    github-cli
-    stow
-    neofetch
-    bat
-    python3
-    unzip
-    tmux
-    rsync
-    picom
-    curl
-    wget
-    xclip
-    scrcpy
-    feh
-    sxiv
-    zoxide
-    gcc
-    clang
-    clang-tools
-    binutils
-    gnumake
-    lxappearance
-    capitaine-cursors
-    tdesktop
-    flameshot
-    rofi
-    htop
-    shfmt
-    stylua
-    nixfmt
-    ytfzf
-    mpv
-    shellcheck
-    emacs
-    st-guss
-    dmenu-guss
-    rustup
-    rust-analyzer
-    zathura
-    bitwarden
-    catgirl
-    spotify
-    deno
-    yarn
-    neovim-nightly
-  ];
-
+  environment = {
+    pathsToLink = [ "/share/zsh" ];
+    systemPackages = with pkgs; [
+      zsh
+      psmisc
+      fish
+      ripgrep
+      sxhkd
+      polybar
+      fd
+      fzf
+      go
+      gopls
+      sumneko-lua-language-server
+      brave
+      firefox
+      exa
+      starship
+      lazygit
+      git
+      github-cli
+      stow
+      neofetch
+      python3
+      unzip
+      tmux
+      rsync
+      picom
+      curl
+      wget
+      xclip
+      scrcpy
+      feh
+      sxiv
+      zoxide
+      gcc
+      clang
+      clang-tools
+      binutils
+      gnumake
+      lxappearance
+      capitaine-cursors
+      tdesktop
+      flameshot
+      rofi
+      htop
+      shfmt
+      stylua
+      nixfmt
+      ytfzf
+      mpv
+      shellcheck
+      st-guss
+      dmenu-guss
+      rustup
+      rust-analyzer
+      zathura
+      bitwarden
+      catgirl
+      deno
+      yarn
+      neovim-nightly
+      cmus
+    ];
+  };
   virtualisation.podman.enable = true;
 
   system.stateVersion = "21.11";
--- overlay.nix
+++ overlay.nix
@@ -10,21 +10,9 @@
   dmenu-guss = super.dmenu.overrideAttrs (oldAttrs: {
     pname = "dmenu-guss";
     version = "0.0.1";
-
     src = fetchTarball {
-         url = "https://github.com/gussdelf/dmenu/archive/master.tar.gz";
+      url = "https://github.com/gussdelf/dmenu/archive/master.tar.gz";
     };
     buildInputs = oldAttrs.buildInputs ++ (with super; [ harfbuzz ]);
-  });
-  neovim-nightly = super.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
-    pname = "neovim-nightly";
-    version = "master";
-    src = builtins.fetchGit {
-      url = https://github.com/neovim/neovim.git;
-    };
-
-    nativeBuildInputs = super.neovim-unwrapped.nativeBuildInputs
-      ++ [ super.tree-sitter ];
-
   });
 })
