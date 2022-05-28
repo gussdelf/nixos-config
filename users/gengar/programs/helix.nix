b4ba5ac937f2508d2461a78e8b50d513c7a89b6f
Gustavo Freitas <gussdelf@gmail.com>
2022-05-28 20:11:57 -0300

    oops

---
+++ users/gengar/programs/helix.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "base16_default_dark";
+      editor = {
+        line-number = "relative";
+        true-color = true;
+        mouse = false;
+        cursor-shape = {
+          insert = "block";
+          normal = "block";
+          select = "underline";
+        };
+        file-picker = { hidden = false; };
+      };
+    };
+    languages = [
+      {
+        name = "nix";
+        auto-format = true;
+      }
+      {
+        name = "rust";
+        auto-format = true;
+      }
+    ];
+  };
+}
