3f36eac26dca2e2ecc39b8bedc930be4b275b3ae
Gustavo Freitas <gussdelf@gmail.com>
2022-06-16 13:33:46 -0300

    neovim home.file recursive

--- users/gengar/programs/neovim.nix
+++ users/gengar/programs/neovim.nix
@@ -1,6 +1,9 @@
 { config, pkgs, overlays, ... }:
 
 {
-  home.file."${config.home.homeDirectory}/.config/nvim".source = ../config/nvim;
+  home.file."${config.home.homeDirectory}/.config/nvim" = {
+    source = ../config/nvim;
+    recursive = true;
+  };
   home = { packages = with pkgs; [ neovim-nightly ]; };
 }
